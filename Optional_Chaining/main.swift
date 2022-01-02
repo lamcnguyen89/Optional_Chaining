21a60df17c76508d3460de5f7d09f289334ffb75
Lam.Nguyen <lam.nguyen@cognizant.com>
2022-01-02 18:44:28 -0500

    New

--- Optional_Chaining/main.swift
+++ Optional_Chaining/main.swift
@@ -7,5 +7,34 @@
 
 import Foundation
 
-print("Hello, World!")
+// MARK: - Optional Chaining
+// Optional chaining is a process for querying and calling properties, methods and subscripts on an optional that might currently be nil.
+// If the optional contains a value, the property, method or subscript call succeeds
+// If the optional is nil, the property, method, or subscript call returns nil.
+// Multiple queries can be chained together, and the entire chain fails gracefully if any link in the chain is nil
 
+// You specify optional chaining by placing a question mark after the optional value.
+
+class Person {
+    var residence : Residence?
+}
+
+class Residence {
+    var numberOfRooms = 1
+}
+
+// If you create a new Person instance, its residence property is by default initialized to nil, by virtue of being an optional. In the ocde below, newguy has a residence property value of nil:
+var newGuy = Person()
+
+// This triggers a runtime error:
+    // let roomCount = newGuy.residence!.numberOfRooms
+// To fix, you have to make Residence() an non-optional value.
+
+// Or you can use optional chaining and a question mark instead of exclamation mark
+if let roomCount = newGuy.residence?.numberOfRooms {
+    print("the New Guy's House has \(roomCount) rooms")
+} else {
+    print("Can't get a number of rooms")
+}
+// Optional chaining will allow error handling.
+// Prints "Can't get the number of rooms"
